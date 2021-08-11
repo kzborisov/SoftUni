d4add44211f5231669c525fdbc93fbdceb05a978
kborisov <kborisov@lely.com>
2021-08-11 16:40:53 +0300

    Exam Preparation - Mirror Words fancy solution

--- Programming Fundamentals With Python (May 2021)/26. Exam Preparation/03. PF Final Exam/02_mirror_words.py
+++ Programming Fundamentals With Python (May 2021)/26. Exam Preparation/03. PF Final Exam/02_mirror_words.py
@@ -1,36 +1,20 @@
 import re
 
-
-def is_palindrome(first, second):
-    if first == second[::-1]:
-        return True
-    return False
-
-
 text = input()
 pattern = r"([@#])(?P<word>[A-Za-z]{3,})(\1)(\1)(?P<mirror>[A-Za-z]{3,})(\1)"
 matches = re.finditer(pattern, text)
-palindromes = {}
-pairs = {}
 
-for match in matches:
-    word = match.group('word')
-    mirror = match.group('mirror')
-    pairs[word] = mirror
-    if is_palindrome(word, mirror):
-        palindromes[word] = mirror
+matches = list(re.finditer(pattern, text))
+mirror_words = [f"{match.group('word')} <=> {match.group('mirror')}"
+                for match in matches if match.group('word') == match.group('mirror')[::-1]]
 
-if pairs:
-    print(f"{len(pairs)} word pairs found!")
-    if palindromes:
-        print("The mirror words are:")
-        result = ""
-        for word, mirror in palindromes.items():
-            result += f"{word} <=> {mirror}, "
-        print(result[:-2])
-    else:
-        print("No mirror words!")
-else:
+if len(matches) == 0:
     print("No word pairs found!")
-    if not palindromes:
-        print("No mirror words!")
+else:
+    print(f"{len(matches)} word pairs found!")
+
+if mirror_words:
+    print("The mirror words are:")
+    print(', '.join(mirror_words))
+else:
+    print("No mirror words!")
