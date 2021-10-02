707090392b482323651b82c19646fd06a3b04aaa
kborisov <kborisov@lely.com>
2021-10-02 09:47:35 +0300

    File Handling - 30.09.2021

--- 3. Python Advanced (September 2021)/3.1 Python Advanced (September 2021)/11. File Handling/01_file_opener.py
+++ 3. Python Advanced (September 2021)/3.1 Python Advanced (September 2021)/11. File Handling/01_file_opener.py
@@ -1,13 +1,7 @@
-# try:
-#     file = open("./text.txt", 'r')
-#     print('File found')
-#     file.close()
-# except FileNotFoundError:
-#     print('File not found')
-import time
-
-for i in range(10):
-    time.sleep(.3)
-    print("\r", end="")
-    print(i, end="")
+try:
+    file = open("./text.txt", 'r')
+    print('File found')
+    file.close()
+except FileNotFoundError:
+    print('File not found')
 
