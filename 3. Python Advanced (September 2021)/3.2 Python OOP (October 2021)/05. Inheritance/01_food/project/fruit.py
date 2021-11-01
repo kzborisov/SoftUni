f3b957ad5ecb245dd05272f4ff2d2c927233ed7a
kborisov <kborisov@lely.com>
2021-11-01 21:09:33 +0200

    Inheritance - 01.11.2021

--- 3. Python Advanced (September 2021)/3.2 Python OOP (October 2021)/05. Inheritance/01_food/project/fruit.py
+++ 3. Python Advanced (September 2021)/3.2 Python OOP (October 2021)/05. Inheritance/01_food/project/fruit.py
@@ -3,5 +3,5 @@ from project.food import Food
 
 class Fruit(Food):
     def __init__(self, name, expiration_date):
+        super().__init__(expiration_date)
         self.name = name
-        self.expiration_date = expiration_date
