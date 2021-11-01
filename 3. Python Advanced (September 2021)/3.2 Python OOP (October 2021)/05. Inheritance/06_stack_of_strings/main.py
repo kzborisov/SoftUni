104eab91c28c55da412519cf084b16e636612bb9
kborisov <kborisov@lely.com>
2021-11-01 11:10:55 +0200

    Inheritance - 01.11.2021

---
+++ 3. Python Advanced (September 2021)/3.2 Python OOP (October 2021)/05. Inheritance/06_stack_of_strings/main.py
@@ -0,0 +1,19 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, element):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return False if self.data else True
+
+    def __str__(self):
+        msg = f"[{', '.join(sorted(self.data, reverse=True))}]"
+        return msg
