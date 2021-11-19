c16a64f02b4a2077c620ac8d5b802e9ee2d497eb
kborisov <kborisov@lely.com>
2021-11-19 20:59:29 +0200

    Exam preparation 22 Aug 2020

---
+++ 3. Python Advanced (September 2021)/3.2 Python OOP (October 2021)/24. Exam Preparation/22.08.2020/main.py
@@ -0,0 +1,19 @@
+from project.rooms.young_couple import YoungCouple
+from project.rooms.young_couple_with_children import YoungCoupleWithChildren
+from project.people.child import Child
+from project.everland import Everland
+
+everland = Everland()
+
+young_couple = YoungCouple("Johnsons", 150, 205)
+
+child1 = Child(5, 1, 2, 1)
+child2 = Child(3, 2)
+young_couple_with_children = YoungCoupleWithChildren("Peterson", 600, 520, child1, child2)
+
+everland.add_room(young_couple)
+everland.add_room(young_couple_with_children)
+
+print(everland.get_monthly_consumptions())
+print(everland.pay())
+print(everland.status())
---
+++ 3. Python Advanced (September 2021)/3.2 Python OOP (October 2021)/24. Exam Preparation/22.08.2020/project/rooms/room.py
@@ -0,0 +1,20 @@
+class Room:
+    def __init__(self, name, budget, members_count):
+        self.family_name = name
+        self.budget = budget
+        self.members_count = members_count
+        self.children = []
+        self.expenses = 0
+
+    @property
+    def expenses(self):
+        return self.__expenses
+
+    @expenses.setter
+    def expenses(self, value):
+        if value < 0:
+            raise ValueError("Expenses cannot be negative")
+        self.__expenses = value
+
+    def calculate_expenses(self, *args):
+        self.expenses = sum(el.get_monthly_expense() for arg in args for el in arg)
